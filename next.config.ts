a64d76bbb18eb793cca338844c90cddc4c4d4af1
sciyen <a885566885566@gmail.com>
2025-05-24 22:57:14 +0800

    Add github page publish action

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/Badminton-Scheduler",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
